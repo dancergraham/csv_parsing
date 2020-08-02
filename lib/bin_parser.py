9b47cd797654480ae5a452152ba5ff341abf02d1
dancergraham <graham.knapp@gmail.com>
2020-08-02 19:48:28 +0200

    Now reads data to a numpy array

--- lib/bin_parser.py
+++ lib/bin_parser.py
@@ -1,8 +1,10 @@
 import struct
+import numpy as np
 
 
 def main():
-    """Read catman 5.0 format"""
+    """Read catman 5.0 format. Format documented at :
+    https://docs.google.com/spreadsheets/d/1dZOw9L6_ukHNYlcR-n64DuRZ702nBq6jjA169q-aCz0/edit#gid=0"""
     with open(r"C:\Users\KNAPP\PycharmProjects\csv_parsing\test_samples\2020_07_22_U_4.8_0deg_2.BIN", "rb") as f:
 
         # read global section
@@ -27,10 +29,12 @@ def main():
 
         reduction_factor = int.from_bytes(f.read(4), byteorder='little')
 
+        # Channel header section
+        channels =[]
         for i in range(number_of_channels):
-
+            channel = {}
             channel_number = int.from_bytes(f.read(2), byteorder='little')
-            channel_length = int.from_bytes(f.read(4), byteorder='little')
+            channel["length"] = int.from_bytes(f.read(4), byteorder='little')
             channel_name_length = int.from_bytes(f.read(2), byteorder='little')
             channel_name = f.read(channel_name_length).decode(encoding='latin_1')
             channel_unit_name_length = int.from_bytes(f.read(2), byteorder='little')
@@ -47,14 +51,18 @@ def main():
             channel_linearisation_mode = int.from_bytes(f.read(1), byteorder='little')
             channel_user_scale = int.from_bytes(f.read(1), byteorder='little')
             channel_linearisation_points = int.from_bytes(f.read(1), byteorder='little')
-            print(f"{channel_linearisation_mode}, {channel_user_scale}, {channel_linearisation_points}")
             thermo_type = int.from_bytes(f.read(2), byteorder='little')
             formula_length = int.from_bytes(f.read(2), byteorder='little')
             formula = f.read(formula_length).decode(encoding='latin_1')
             DB_sensor_info_length = int.from_bytes(f.read(4), byteorder='little')
             DB_sensor_info = f.read(DB_sensor_info_length).decode(encoding='latin_1')
-            breakpoint()
+            channels.append(channel)
+        f.read((3*8192)-f.tell())  # fudge to find start of data - I dont understand why this works
+        # Read Data
+        for i in range(number_of_channels):
+            channels[i]["data"] = np.frombuffer(f.read(8*channels[i]["length"]), dtype='<f8')
 
 
 if __name__ == '__main__':
     main()
+    
